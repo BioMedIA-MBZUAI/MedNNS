085f73504f23800e8384ad2becb28425cca6f86d
meclotfi <lotfi.mecharbat@mbzuai.ac.ae>
2025-09-03 14:03:50 +0400

    init

---
+++ dataload/dataloader.py
@@ -0,0 +1,81 @@
+
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import random
+import torch
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
+from medmnist import INFO, Evaluator
+from medmnist import DermaMNIST, PneumoniaMNIST, PathMNIST, RetinaMNIST, BreastMNIST , BloodMNIST, ChestMNIST, OCTMNIST, OrganCMNIST, OrganSMNIST, OrganAMNIST, TissueMNIST
+import torchvision.transforms as transforms
+import wandb
+import torch.nn.functional as F
+import os
+from torchvision import transforms
+import numpy as np
+
+# Mapping dataset names to their respective MedMNIST classes
+data_class_mapping = {
+    'PathMNIST': PathMNIST,# o
+    'ChestMNIST': ChestMNIST,# o
+    'DermaMNIST': DermaMNIST, # X
+    'OCTMNIST': OCTMNIST,# o
+    'PneumoniaMNIST': PneumoniaMNIST,# o
+    'RetinaMNIST': RetinaMNIST,# X
+    'BreastMNIST': BreastMNIST,# X
+    'BloodMNIST': BloodMNIST,# X
+    'TissueMNIST': TissueMNIST,# o
+    'OrganAMNIST': OrganAMNIST,# o
+    'OrganCMNIST': OrganCMNIST, # X
+    'OrganSMNIST': OrganSMNIST # X
+}
+
+def get_dataloaders(dataset_name, batch_size=32):
+    """
+    Load MedMNIST dataset based on the dataset name and return dataloaders.
+
+    Args:
+        dataset_name (str): Name of the dataset to load.
+        batch_size (int): Batch size for the dataloaders.
+
+    Returns:
+        train_loader, val_loader, test_loader
+    """
+    if dataset_name not in data_class_mapping:
+        raise ValueError(f"Invalid dataset name '{dataset_name}'. Must be one of {list(data_class_mapping.keys())}.")
+
+    DatasetClass = data_class_mapping[dataset_name]
+
+    # Define transforms for training and validation/test
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),  # Random crop to 224x224
+        transforms.RandomHorizontalFlip(),  # Random horizontal flip
+        transforms.ToTensor(),             # Convert image to tensor
+        transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize with mean and std
+    ])
+
+    val_test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),      # Resize to 224x224
+        transforms.ToTensor(),             # Convert image to tensor
+        transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize with mean and std
+    ])
+
+    # Download and prepare data
+    train_dataset = DatasetClass(split='train', transform=train_transform, download=True)
+    val_dataset = DatasetClass(split='val', transform=val_test_transform, download=True)
+    test_dataset = DatasetClass(split='test', transform=val_test_transform, download=True)
+
+    # Dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    # Data information
+    data_info = {
+        'num_classes': len(np.unique(train_dataset.labels)),
+        'image_size': train_dataset[0][0].shape,  # Shape of the
+        'name': dataset_name
+    }
+
+    return  train_dataset,val_loader, test_loader, data_info
